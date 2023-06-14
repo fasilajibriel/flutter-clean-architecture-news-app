81b5943045e307c07604dd162d812c2594f9f161
fasilajibriel <fasilajibriel@gmail.com>
2023-06-15 00:13:05 +0300

    define data_state

---
+++ lib/core/resources/data_state.dart
@@ -0,0 +1,16 @@
+import 'package:dio/dio.dart';
+
+abstract class DataState<T> {
+  final T? data;
+  final DioException? dioException;
+
+  const DataState({this.data, this.dioException});
+}
+
+class DataSuccess<T> extends DataState {
+  const DataSuccess(T data) : super(data: data);
+}
+
+class DataFailed<T> extends DataState {
+  const DataFailed(DioException dioException) : super(dioException: dioException);
+}
